ff790efa9450d74b979dd4f110621498828a3388
Katie Jahanmir <katietj@gmail.com>
2018-10-06 13:11:56 -0700

    completed fibonacci

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,5 +5,19 @@
 # ....
 # e.g. 6th fibonacci number is 8
 def fibonacci(n)
-  raise NotImplementedError
+  if n == nil || n < 0
+    raise ArgumentError
+  elsif
+    n < 2
+    return n
+  else
+    a = 0
+    b = 1
+    n.times do
+      temp = a
+      a = b
+      b = temp + b
+    end
+    return  a
+  end
 end
