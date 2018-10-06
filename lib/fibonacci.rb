f14870f1ac4309c1ab222d20015e21c1c0d27c61
Valerie Gidzinski <valgidzi@gmail.com>
2018-10-05 22:41:41 -0700

    Passes all tests

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,5 +5,25 @@
 # ....
 # e.g. 6th fibonacci number is 8
 def fibonacci(n)
-  raise NotImplementedError
+
+  raise ArgumentError if n.nil? || n < 0
+
+  a = Array.new(n, 0)
+
+  a[0] = 0
+  a[1] = 1
+
+  i = 2
+
+  while i <= n
+    a[i] = a[i-1] + a[i-2]
+    i += 1
+  end
+
+  return a[n]
 end
+
+# Time complexity: linear
+#  Given an integer of value n, the time complexity is O(n). The runtime is directly proportionate to the value of n.
+# Space complexity: linear
+#  Given an integer of value n, the space complexity is O(n). The size of the array is set by the value of n.
