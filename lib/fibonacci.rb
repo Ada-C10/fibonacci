d93e1c41b72e969308a4e6d561c50c93ff863a24
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-07 21:24:47 -0700

    Fibonacci method

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,5 +5,30 @@
 # ....
 # e.g. 6th fibonacci number is 8
 def fibonacci(n)
-  raise NotImplementedError
+  if n == nil || n < 0
+    raise ArgumentError.new("Invalid value")
+  elsif n == 0
+    return 0
+  else
+    array = []
+    array << 0
+    array << 1
+    x = 0
+    y = x + 1
+    until y == n
+
+      first = array[x]
+      second = array[y]
+      sum = first + second
+      array << sum
+
+      x += 1
+      y += 1
+    end
+  end
+  return array[n]
+  # raise NotImplementedError
 end
+
+test = fibonacci(7)
+puts "#{test}"
--- specs/fibonacci_spec.rb
+++ specs/fibonacci_spec.rb
@@ -11,15 +11,15 @@ describe "fibonacci" do
     it "fibonacci of 3" do
       fibonacci(3).must_equal 2
     end
-
+    #
     it "fibonacci of 8" do
       fibonacci(8).must_equal 21
     end
-
+    #
     it "fibonacci of 12" do
       fibonacci(12).must_equal 144
     end
-
+    #
     it "fibonacci of 6" do
       fibonacci(6).must_equal 8
     end
@@ -27,15 +27,15 @@ describe "fibonacci" do
 
   # check for edge cases
   describe "edge cases" do
-    # if the parameter is an object, check for nil
+  #   # if the parameter is an object, check for nil
     it "nil object is not an integer" do
       proc {fibonacci(nil)}.must_raise ArgumentError
     end
-
+  #
     it "negative input" do
       proc {fibonacci(-9)}.must_raise ArgumentError
     end
-
+  #
     it "zero input" do
       fibonacci(0).must_equal 0
     end
