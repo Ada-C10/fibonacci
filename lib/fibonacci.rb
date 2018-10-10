3feb074e3f882d7ef80cf4b1b3b248532f855ee0
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-10-09 20:33:50 -0700

    Fib SUCCESS

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,5 +5,19 @@
 # ....
 # e.g. 6th fibonacci number is 8
 def fibonacci(n)
-  raise NotImplementedError
+
+  raise ArgumentError unless ( n.class == Integer && n >= 0 )
+
+  fib = 0
+  next_fib = 1
+
+  n.times do
+    next_fib += fib
+    fib = next_fib - fib
+  end
+
+  return fib
 end
+
+# Space Complexity: O(1) constant
+# Time Complexity: O(n) linear
