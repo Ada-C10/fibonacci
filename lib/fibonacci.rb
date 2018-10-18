bfe6a578fd7181a9bbd041c0625ece370d2cda48
Pushpa Agr <pushpaagr108@gmail.com>
2018-10-17 21:33:15 -0700

    completed fibonacci

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -5,5 +5,35 @@
 # ....
 # e.g. 6th fibonacci number is 8
 def fibonacci(n)
-  raise NotImplementedError
+
+  if n == 0
+    return 0
+  elsif n == 1
+    return 1
+  elsif n == 2
+    return 1
+  elsif n == nil
+    raise ArgumentError.new ("no")
+  elsif n < 0
+    raise ArgumentError.new ("no")    
+  end
+
+
+  start = 0
+  next_num = 1
+  sum = start + next_num
+
+  n = n - 2
+  i = 0
+
+  while i < n
+    start = next_num
+    next_num = sum
+    sum = start + next_num
+    i += 1
+  end
+
+  return sum
+
+  # raise NotImplementedError
 end
